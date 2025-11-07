f0b452dbc119d3eb067e734b031f39690ecabe2b
Bobokalonov__J <jahongirbobokalonov53@gmail.com>
2025-11-07 09:32:45 +0500

    Change file bobokalonov

---
+++ js/getDirections.js
@@ -0,0 +1,12 @@
+getDirections();
+
+async function getDirections() {
+    const response = await fetch("/getDirections");
+    const data = await response.json();
+    let directions = [];
+    data.forEach(direction => {
+        directions.push(`<option value="${direction.id}">${direction.title_ru}</option>`);
+    });
+    document.getElementById("directionsList").innerHTML =directions.join("\n");
+    console.log(data);
+}
